5378c70cb7a3f05b79555d5e6b448e9058797b35
Olesia <kravchenkoolesya11@gmail.com>
2024-06-29 15:10:26 +0200

    Added project

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+
+class Program{
+    static void Start(){
+        Application.Init();
+
+        Window main = new Window("My first GTK# Application!");
+        main.Resize(1400, 800);
+
+        main.DeleteEvent += delegate { Application.Quit(); };
+
+        main.Show();
+
+        Application.Run();
+    }
+
+    static void Main(string[] args){
+        Start();
+    }
+}
